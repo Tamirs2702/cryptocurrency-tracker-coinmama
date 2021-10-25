e15f7d14ffb423b2435e76d24deba3b67206531a
Tamir Shmueli <dojo4dojo@gmail.com>
2021-10-25 03:15:21 +0300

    Updates

---
+++ precache-manifest.7f87c6086796fd5977e01919443b3d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439160973852f0257a38835cb4e7ebdf",
+    "url": "/cryptocurrency-tracker-coinmama/index.html"
+  },
+  {
+    "revision": "c2eae6cf7004cda76311",
+    "url": "/cryptocurrency-tracker-coinmama/static/css/main.2b07b6aa.chunk.css"
+  },
+  {
+    "revision": "af2cc4927ee53754f4c8",
+    "url": "/cryptocurrency-tracker-coinmama/static/js/2.29edc68e.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/cryptocurrency-tracker-coinmama/static/js/2.29edc68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2eae6cf7004cda76311",
+    "url": "/cryptocurrency-tracker-coinmama/static/js/main.5a59b4a3.chunk.js"
+  },
+  {
+    "revision": "c050754d29b776697216",
+    "url": "/cryptocurrency-tracker-coinmama/static/js/runtime-main.84ca0076.js"
+  }
+]);
